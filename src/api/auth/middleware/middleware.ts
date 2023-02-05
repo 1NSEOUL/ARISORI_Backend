7e66679642a27b38970d09227b69d951cf4cf22f
Ubinquitous <ubinquitous1@gmail.com>
2023-02-05 19:34:28 +0900

    ADD : Login

---
+++ src/api/auth/middleware/middleware.ts
@@ -0,0 +1,20 @@
+import jwt, { Secret } from 'jsonwebtoken'
+import { UserRepository } from '@database/repository/Repository'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const SECRET_KEY: Secret | undefined = process.env.SECRET_KEY
+
+const isLogin = async (token: string) => {
+	try {
+		if (!SECRET_KEY) throw new Error('SECRET_KEY NOT FOUND')
+		const decodejwt = JSON.parse(JSON.stringify(<string>jwt.verify(token, SECRET_KEY)))
+		const code: number = decodejwt.code
+		return await UserRepository.findOneBy({ id: code })
+	} catch (e) {
+		return null
+	}
+}
+
+export { isLogin }

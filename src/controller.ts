07d8016af08730475d93757342e2f4ece7331bae
Ubinquitous <ubinquitous1@gmail.com>
2023-02-05 19:33:49 +0900

    ADD : auth, chat controller

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 const router = express.Router()
+import authController from '@src/api/auth/auth.controller'
+import chatController from '@src/api/chat/chat.controller'
 
-router.get('/')
+router.use('/oauth', authController)
+router.use('/chat', chatController)
 
 export default router

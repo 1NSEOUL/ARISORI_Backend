dfb4663f77fa59243f9c1f8ef3432a48bec487b8
Ubinquitous <ubinquitous1@gmail.com>
2023-02-06 10:11:27 +0900

    UPDATE : 경로, message 변경

--- src/api/chat/getChat.ts
+++ src/api/chat/getChat.ts
@@ -1,13 +1,13 @@
-import { ChatRepository } from '@src/database/repository/Repository'
-import { InternalServerException } from '@src/global/exception/exception'
-import { GlobalResponseDTO } from '@src/global/res/DTO/GlobalResponseDTO'
-import { GlobalService } from '@src/global/res/GlobalService'
+import { ChatRepository } from '../../database/repository/Repository'
+import { InternalServerException } from '../../global/exception/exception'
+import { GlobalResponseDTO } from '../../global/res/DTO/GlobalResponseDTO'
+import { GlobalService } from '../../global/res/GlobalService'
 import { NextFunction, Request, Response } from 'express'
 
 const getChat = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const AllPost = await ChatRepository.find()
-		const Dto = new GlobalResponseDTO(200, '성공', AllPost)
+		const Dto = new GlobalResponseDTO(200, 'get chatting success', AllPost)
 		GlobalService(res, Dto)
 	} catch (e) {
 		return next(new InternalServerException())

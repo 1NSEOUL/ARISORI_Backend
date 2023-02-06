6fda62b0b6f49f940c311960bdbf1744ee4f00bc
Ubinquitous <ubinquitous1@gmail.com>
2023-02-06 10:08:29 +0900

    FIX : 경로

--- src/controller.ts
+++ src/controller.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
-import authController from '@src/api/auth/auth.controller'
-import chatController from '@src/api/chat/chat.controller'
+import authController from './api/auth/auth.controller'
+import chatController from './api/chat/chat.controller'
 
 router.use('/oauth', authController)
 router.use('/chat', chatController)
